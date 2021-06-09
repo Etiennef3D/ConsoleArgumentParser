088dbeb86dce4e1b79a999eb5778a24f997ddb02
Etiennef3D <etienne.fdarcier@hotmail.fr>
2021-06-09 17:07:00 +0200

    Created project

---
+++ ConsoleArgumentParser_Prj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleArgumentParser_Prj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
