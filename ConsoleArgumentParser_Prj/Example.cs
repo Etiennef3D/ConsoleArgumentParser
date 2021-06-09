6b31c49cab79a2456bbb90e02a3a380aa0e82576
Etiennef3D <etienne.fdarcier@hotmail.fr>
2021-06-09 17:47:01 +0200

    Added IsValueExists Method instead of TryGetValue everywhere, and added a few comments

---
+++ ConsoleArgumentParser_Prj/Example.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using ConsoleArgumentParser.src;
+
+namespace ConsoleArgumentParser
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Just faking arguments for the sake of example
+            args = new string[] { "-v", "-d" };
+
+            ArgumentParser _parser = new ArgumentParser();
+
+            //Testing parameter -v for verbosity
+            _parser.AddArgument("-v", () =>
+            {
+                Console.SetOut(TextWriter.Null);
+            });
+
+            //If my previous arg was successfully registered,
+            //nothing should print on the console
+            _parser.AddArgument("-d", () => { Console.WriteLine("dddddddd"); });
+
+            //Finally parse
+            _parser.Parse(args);
+        }
+    }
+}
--- ConsoleArgumentParser_Prj/src/ArgumentParser.cs
+++ ConsoleArgumentParser_Prj/src/ArgumentParser.cs
@@ -1,42 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ConsoleArgumentParser_Prj.src
+namespace ConsoleArgumentParser.src
 {
     public class ArgumentParser
     {
-        private string[] _arguments;
         private Dictionary<string, Action> _actions = new Dictionary<string, Action>();
 
         public ArgumentParser() { }
 
-        public ArgumentParser(string[] _args)
-        {
-            this._arguments = _args;
-        }
-
+        /// <summary>
+        /// Adding an argument to invoke it later. You can register any string you need.
+        /// </summary>
         public void AddArgument(string _initiator, Action _invocation)
         {
-            Console.WriteLine($"Parsing value: {_initiator}, with invocation: {_invocation}.");
-
             //Checking if argument is not already set
             //If so set it an action
-            if(_actions.TryGetValue(_initiator, out Action _myAction))
+            if (IsValueExists(_initiator))
             {
-                //Already set
                 _actions[_initiator] = _invocation;
+                Console.WriteLine("Argument successfully replaced.");
             }
             else
             {
-                //Add an entry in the list
+                //Add an entry in the dictionnary
                 _actions.Add(_initiator, _invocation);
+                Console.WriteLine("Argument successfully added.");
             }
         }
 
-        public void Parse(string[] _args)
-        {
 
+        /// <summary>
+        /// If you want to remove an argument for whatever reason.
+        /// </summary>
+        /// <param name="_initiatorKey"></param>
+        public void RemoveArgument(string _initiatorKey)
+        {
+            if (IsValueExists(_initiatorKey))
+            {
+                _actions.Remove(_initiatorKey);
+                Console.WriteLine("Argument successfully removed.");
+            }
+            else
+            {
+                Console.WriteLine("Key not recognized.");
+            }
         }
 
+
+        /// <summary>
+        /// Simplify access to test a key in the dictionnary.
+        /// </summary>
+        /// <param name="_key"></param>
+        /// <returns></returns>
+        private bool IsValueExists(string _key) => _actions.TryGetValue(_key, out Action _invocation) ? true : false;
+
+
+        /// <summary>
+        /// Check if the key value exists before invoking its value.
+        /// </summary>
+        public void Parse(string[] _args)
+        {
+            foreach (string _s in _args)
+            {
+                if (IsValueExists(_s))
+                {
+                    //Execute existing action
+                    _actions[_s]();
+                }
+            }
+        }
     }
 }
